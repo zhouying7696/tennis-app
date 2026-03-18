2a00fc7aed9faddb8bd10a3bab55f4eba0c6b98c
Your Name <356664116@qq.com>
2026-03-18 18:30:42 +0800

    Add PostgreSQL support for Railway deployment

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,23 @@
 const { Sequelize } = require('sequelize');
 
-// 使用SQLite作为嵌入式数据库
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './tennis_app.db',
-  logging: false
-});
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
+  });
+} else {
+  sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './tennis_app.db',
+    logging: false
+  });
+}
 
 module.exports = sequelize;
